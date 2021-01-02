86df9b189f38d9a9e4330265db870fcb81283a26
dhantonio <antonio.abreu@digiheart.net>
2021-01-02 02:46:23 +0000

    KOTLIN 2 - CICLO DE VIDA

--- app/src/main/java/com/example/teste/MainActivity.kt
+++ app/src/main/java/com/example/teste/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.teste
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) //diz qual o ficheiiro de layout que tera o conteudo para esta atividade
+        setContentView(R.layout.activity_main)
+        Log.d(  "****TAG ", "onCreate")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("****TAG ", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("****TAG ", "onStop")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("****TAG ", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("****TAG ", "onResume")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("****TAG ", "onDestroy")
     }
 }
 
