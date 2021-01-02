935530186111a50be4ca7def7d3cf7a74a77d389
dhantonio <antonio.abreu@digiheart.net>
2021-01-02 17:13:49 +0000

    KOTLIN 7 - CONTROLOS COMUNS

--- app/src/main/java/com/example/teste/MainActivity.kt
+++ app/src/main/java/com/example/teste/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.teste
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText(this, R.string.welcome, Toast.LENGTH_SHORT).show()
+    }
+
+    fun button1(view: View) {
+
+        var edit1 = findViewById<EditText>(R.id.editTExtTextPersonName)
+        Toast.makeText(this, edit1.text, Toast.LENGTH_SHORT).show()
+        findViewById<TextView>(R.id.textView6).setText(edit1.text)
+    }
+
+    fun button2(view: View) {
+        Toast.makeText(this, "button2", Toast.LENGTH_LONG).show()
     }
 }
 
