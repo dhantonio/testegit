9271366692fa08807b01c39380e5f4cbec25b720
dhantonio <antonio.abreu@digiheart.net>
2021-01-02 02:55:39 +0000

    KOTLIN 3 - MULTILINGUA

--- app/src/main/java/com/example/teste/MainActivity.kt
+++ app/src/main/java/com/example/teste/MainActivity.kt
@@ -3,37 +3,13 @@ package com.example.teste
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(  "****TAG ", "onCreate")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d("****TAG ", "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d("****TAG ", "onStop")
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d("****TAG ", "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d("****TAG ", "onResume")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("****TAG ", "onDestroy")
+        Toast.makeText(this, R.string.welcome, Toast.LENGTH_SHORT).show()
     }
 }
 
